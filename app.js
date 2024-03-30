cc22d6b6bb310722e5d2fff462bc083685f45bec
soham <jambhwadekarsoham@gmail.com>
2024-03-30 17:18:59 +0530

    fix #3.1

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '500mb' }));
+app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
 
 app.use(fileUpload());
 
